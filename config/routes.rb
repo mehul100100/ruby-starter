1a61bac5a4917412d6d03278ab2f5967e70e8c3e
mk12343194 <29296084-mk12343194@users.noreply.replit.com>
2024-02-19 08:32:47 +0000

    Initial Repository

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  resources :microposts
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "application#hello"
+  root "users#index"
+end
